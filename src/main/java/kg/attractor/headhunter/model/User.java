b0dcdd6107a0263af765483dcf86dfe197789ffc
Nursultan Beishekeev <nursultanzynwa@gmail.com>
2024-03-06 19:13:39 +0600

    Created User.java model

---
+++ src/main/java/kg/attractor/headhunter/model/User.java
@@ -0,0 +1,13 @@
+package kg.attractor.headhunter.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String avatar;
+    private AccountType accountType;
+}
