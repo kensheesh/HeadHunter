599979404082ff9a145f12150a031f843edafcfa
Nursultan Beishekeev <nursultanzynwa@gmail.com>
2024-03-06 15:18:36 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/headhunter/HeadHunterApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.headhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadHunterApplication.class, args);
+    }
+
+}
